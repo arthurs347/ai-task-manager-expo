d291ea829ba0ec4627a5cdb88f2bf2e86b400186
Arthur Sanchez <arthurs347251@gmail.com>
2025-08-09 00:52:22 -0700

    Configured project and apis to work on dev environment. Need to fix prod env vars.

---
+++ app.config.ts
@@ -0,0 +1,33 @@
+import {ConfigContext, ExpoConfig} from "@expo/config";
+
+const IS_DEV = process.env.APP_ENV === 'development';
+
+const getUniqueIdentifier = () => {
+    if (IS_DEV) return 'com.arthurs347.aitaskmanager.dev';
+
+    return 'com.arthurs347.aitaskmanager';
+};
+
+const getAppName = () => {
+    if (IS_DEV) return 'Ai Task (Dev)';
+
+    return 'Ai Task';
+};
+export default ({ config }: ConfigContext): ExpoConfig => ({
+    ...config,
+    name: getAppName(),
+    slug: config.slug!,
+    ios: {
+        ...config.ios,
+        bundleIdentifier: getUniqueIdentifier(),
+    },
+    android: {
+        ...config.android,
+        package: getUniqueIdentifier(),
+    },
+    extra: {
+        ...config.extra,
+        baseUrl: process.env.APP_URL,
+    }
+
+});
